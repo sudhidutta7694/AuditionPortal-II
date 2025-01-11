e11b6fb413d6f4e138134041f8359cf5ac02c93b
Arkaprava Dhar <raj2003dhar@gmail.com>
2025-01-11 14:00:46 +0530

    Database setup

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 const { DataTypes } = require('sequelize');
 require('dotenv').config();
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+//const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json');
 const sequelize = new Sequelize(config.database,config.username,config.password,config) 
 //const sequelize = new Sequelize(process.env.ELEPHANTSQL);
 const models = [
